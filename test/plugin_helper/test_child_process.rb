675c09846ac25b023cb77fce474d047dc1f4235f
Daijiro Fukuda <fukuda@clear-code.com>
2025-03-05 16:16:19 +0900

    test

--- test/plugin_helper/test_child_process.rb
+++ test/plugin_helper/test_child_process.rb
@@ -317,6 +317,7 @@ class ChildProcessTest < Test::Unit::TestCase
     end
   end
 
+  data(a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10)
   test 'can execute external command many times, which finishes immediately' do
     ary = []
     arguments = ["okay"]

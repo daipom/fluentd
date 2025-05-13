43bb6919d023f97ed614ac91db1ae2d3052029a5
Daijiro Fukuda <fukuda@clear-code.com>
2025-05-13 18:59:51 +0900

    test

--- test/scripts/windows_service_test.ps1
+++ test/scripts/windows_service_test.ps1
@@ -10,3 +10,7 @@ Select-String -Path "fluent.conf" -SimpleMatch -Quiet -Pattern "foo", "bar", "bi
 
 Select-String -Path "fluent.conf" -Pattern "[warn]", "[error]", "[fatal]" -SimpleMatch -Quiet
 Select-String -Path "fluent.conf" -SimpleMatch -Quiet -Pattern "[warn]", "[error]", "[fatal]"
+
+Select-String -Path "fluent.conf" -SimpleMatch -Quiet -Pattern "warn", "error", "fatal"
+Select-String -Path "fluent.conf" -SimpleMatch -Quiet -Pattern "[warn]", "[fatal]"
+Select-String -Path "fluent.conf" -SimpleMatch -Quiet -Pattern "[foo]", "[bar]", "[boo]"
